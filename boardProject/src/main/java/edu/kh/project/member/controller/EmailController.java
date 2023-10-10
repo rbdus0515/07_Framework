0bac64bf0dc3347fbc89f762616d278f5ac00811
rbdus0515 <dnqkd707@gmail.com>
2023-10-10 16:03:15 +0900

    up1

---
+++ boardProject/src/main/java/edu/kh/project/member/controller/EmailController.java
@@ -0,0 +1,23 @@
+package edu.kh.project.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.kh.project.member.model.service.EmailService;
+
+@Controller
+@RequestMapping("/sendEmail")
+public class EmailController {
+	
+	@Autowired
+	private EmailService service;
+	
+	@GetMapping("/signUp")
+	@ResponseBody
+	public int singUp(String email) {
+		return service.signUp(email, "회원 가입");
+	}
+}
--- boardProject/src/main/webapp/resources/js/member/signUp.js
+++ boardProject/src/main/webapp/resources/js/member/signUp.js
@@ -349,9 +349,76 @@ memberTel.addEventListener("input", ()=>{
 // --------------------- 이메일 인증 ---------------------
 
 // 인증번호 발송
+const sendAuthKeyBtn = document.getElementById("sendAuthKeyBtn");
+const authKeyMessage = document.getElementById("authKeyMessage");
+let authTimer;
+let authMin = 4;
+let authSec = 59;
+
+// 인증번호를 발송한 이메일 저장
+let tempEmail;
+
+sendAuthKeyBtn.addEventListener("click", function(){
+    authMin = 4;
+    authSec = 59;
+
+    checkObj.authKey = false;
+
+    if(checkObj.memberEmail){ // 중복이 아닌 이메일인 경우
 
 
-// 인증 확인
+        /* fetch() API 방식 ajax */
+        fetch("/sendEmail/signUp?email="+memberEmail.value)
+        .then(resp => resp.text())
+        .then(result => {
+            if(result > 0){
+                console.log("인증 번호가 발송되었습니다.")
+                tempEmail = memberEmail.value;
+            }else{
+                console.log("인증번호 발송 실패")
+            }
+        })
+        .catch(err => {
+            console.log("이메일 발송 중 에러 발생");
+            console.log(err);
+        });
+        
+
+        alert("인증번호가 발송 되었습니다.");
+
+        
+        authKeyMessage.innerText = "05:00";
+        authKeyMessage.classList.remove("confirm");
+
+        authTimer = window.setInterval(()=>{
+													// 삼항연산자  :  조건 	  ?   	true : false
+            authKeyMessage.innerText = "0" + authMin + ":" + (authSec < 10 ? "0" + authSec : authSec);
+            
+            // 남은 시간이 0분 0초인 경우
+            if(authMin == 0 && authSec == 0){
+                checkObj.authKey = false;
+                clearInterval(authTimer);
+                return;
+            }
+
+            // 0초인 경우
+            if(authSec == 0){
+                authSec = 60;
+                authMin--;
+            }
+
+
+            authSec--; // 1초 감소
+
+        }, 1000)
+
+    } else{
+        alert("중복되지 않은 이메일을 작성해주세요.");
+        memberEmail.focus();
+    }
+});
+
+
 
 
 
